30f9e3d2686d4398e5fca5b5db7bdb265868ee9c
ahmad <ddddd@zhorachu.com>
2017-11-20 23:19:50 +0500

    =initialized yarn, added babel presets react and env, set src and destination app.js files

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,12 @@
+"use strict";
+
 console.log("App.js is running");
 
 // JSX - Javascript XML
-var template = <p>This is JSX from app.js!</p>;
+var template = React.createElement(
+  "h1",
+  null,
+  "Random Decision"
+);
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log("App.js is running");
+
+// JSX - Javascript XML
+var template = <h1>Random Decision</h1>;
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
