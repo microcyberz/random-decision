73e1ef0e0cb266446c1219b36c9e58045385aa32
ahmad <ddddd@zhorachu.com>
2017-11-21 18:43:47 +0500

    =injecting variables and obj.properties into jsx

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,66 @@
 
 console.log("App.js is running");
 
-// JSX - Javascript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "Random Decision"
+var app = {
+    title: "Random Decision",
+    subtutle: "Put your life in computer's hand"
+
+    // JSX - Javascript XML
+};var template = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        app.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        app.subtutle
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+var user = {
+    name: "Ahmad Ali",
+    age: 21,
+    location: "Mansehra"
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        user.name
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: ",
+        user.age
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: ",
+        user.location
+    )
 );
 var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,34 @@
 console.log("App.js is running");
 
+var app = {
+    title: "Random Decision",
+    subtutle: "Put your life in computer's hand"
+}
+
 // JSX - Javascript XML
-var template = <h1>Random Decision</h1>;
+var template = (
+<div>
+    <h1>{app.title}</h1>
+    <p>{app.subtutle}</p>
+    <ol>
+        <li>Item one</li>    
+        <li>Item two</li>    
+    </ol>
+</div>
+);
+
+var user = {
+    name: "Ahmad Ali",
+    age: 21,
+    location: "Mansehra"
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(template, appRoot); 
