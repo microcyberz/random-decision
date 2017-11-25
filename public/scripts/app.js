0383ddf0c69088bca013cb96480c3af79abf11b7
ahmad <ddddd@zhorachu.com>
2017-11-25 12:17:33 +0500

    Making Random decision

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,6 +8,7 @@ var app = {
     options: []
 };
 
+// When item is inserted
 var onFormSubmit = function onFormSubmit(e) {
     e.preventDefault(); //stop sending data in link and reloading whole page
     var option = e.target.elements.option.value; // the input value of form
@@ -18,13 +19,21 @@ var onFormSubmit = function onFormSubmit(e) {
     }
 };
 
+// Making the list empty
 var onRemoveAll = function onRemoveAll() {
     app.options = [];
     render();
 };
 
+// Picking Random Option
+var onMakeDecision = function onMakeDecision() {
+    var randomNum = Math.floor(Math.random() * app.options.length);
+    var option = app.options[randomNum];
+    console.log(option);
+};
 var appRoot = document.getElementById('app');
 
+// Rendering JSX on the page
 var render = function render() {
     var template = React.createElement(
         "div",
@@ -45,28 +54,25 @@ var render = function render() {
             app.options.length > 0 ? "Here are your options" : "No Options"
         ),
         React.createElement(
-            "p",
-            null,
-            app.options.length
+            "button",
+            { disabled: app.options.length === 0, onClick: onMakeDecision },
+            "What Should I Do?"
         ),
         React.createElement(
             "button",
-            { onClick: onRemoveAll },
+            { disabled: app.options.length === 0, onClick: onRemoveAll },
             "Remove All"
         ),
         React.createElement(
             "ol",
             null,
-            React.createElement(
-                "li",
-                null,
-                "Item 1"
-            ),
-            React.createElement(
-                "li",
-                null,
-                "Item 2"
-            )
+            app.options.map(function (item) {
+                return React.createElement(
+                    "li",
+                    { key: item },
+                    item
+                );
+            })
         ),
         React.createElement(
             "form",
--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const app = {
     options: []
 };
 
+
+// When item is inserted
 const onFormSubmit = (e) =>{
     e.preventDefault(); //stop sending data in link and reloading whole page
     const option = e.target.elements.option.value; // the input value of form
@@ -16,24 +18,35 @@ const onFormSubmit = (e) =>{
     }
 }
 
+// Making the list empty
 const onRemoveAll = () => {
     app.options = [];
     render();
 }
 
+// Picking Random Option
+const onMakeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.options.length);
+    const option = app.options[randomNum];
+    console.log(option);
+};
 const appRoot = document.getElementById('app');
 
+// Rendering JSX on the page
 const render = () => {
     const template = (
         <div>
             <h1>{app.title}</h1>
             {app.subtitle && <p>{app.subtitle}</p>}
             <p>{app.options.length > 0 ? "Here are your options": "No Options"}</p>
-            <p>{app.options.length}</p>
-            <button onClick={onRemoveAll}>Remove All</button>
+
+            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I Do?</button>
+            <button disabled={app.options.length === 0} onClick={onRemoveAll}>Remove All</button>
+            
             <ol>
-                <li>Item 1</li>
-                <li>Item 2</li>
+                {
+                    app.options.map((item) => <li key={item}>{item}</li>)
+                }
             </ol>
     
             <form onSubmit={onFormSubmit}>
