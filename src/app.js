b788472f660eaa969d91fc757366b86e525302aa
ahmad <ddddd@zhorachu.com>
2017-12-11 01:22:54 +0500

    imported npm modules and set up babael with webpack

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 // import "./utils";
 import {square, add} from "./utils";
 import isSenior, {isAdult, canDrink} from "./person";
+import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 console.log("pp.js is Running...");
 
@@ -12,3 +15,8 @@ console.log("====== PERSON ======");
 console.log(isAdult(21));
 console.log(canDrink(21));
 console.log(isSenior(65));
+
+console.log("====== VALIDATOR, React, ReactDOM (npm) ======");
+console.log(validator.isEmail('asd@asd.com'))
+const template = <p>JSX template rendered</p>
+ReactDOM.render(template,document.getElementById('app'));
