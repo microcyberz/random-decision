430e67a2e3e99f4742804e7a61e042c91e8a0fc2
ahmad <ddddd@zhorachu.com>
2017-12-14 01:38:16 +0500

    added live server

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,8 @@ module.exports = {
         }]
     },
 
-    devtool: 'cheap-module-eval-source-map'
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+    contentBase: path.join(__dirname, 'public')
+    }
 };
