f23f4dca5b9ca0277b8716876e05a41b91c460f0
ahmad <ddddd@zhorachu.com>
2017-12-17 01:34:17 +0500

    es6 class properties

--- src/components/RandomDecision.js
+++ src/components/RandomDecision.js
@@ -4,18 +4,11 @@ import Action from './Action';
 import Header from './Header';
 import Options from './Options';
 
-
 export default class RandomDecision extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.state = {
-            options: []
-        };
-    }
+    
+    state = {
+        options: []
+    };
 
     componentDidMount(){
         try {
@@ -41,31 +34,31 @@ export default class RandomDecision extends React.Component{
     }
 
     // remove all
-    handleDeleteOptions(){
+    handleDeleteOptions = () => {
         this.setState(()=> ({options: []}));          
-    }
+    };
 
     // remove single
-    handleDeleteOption(optionToRemove){
+    handleDeleteOption = (optionToRemove) => {
         this.setState((prevState) => ({
             options: prevState.options.filter((option) => optionToRemove !== option)
         }));
-    }
+    };
 
-    handlePick(){
+    handlePick = () => {
         const randomNum = Math.floor(Math.random() * this.state.options.length);
         const option = this.state.options[randomNum];
         alert(option);
-    }
+    };
 
-    handleAddOption(option){
+    handleAddOption = (option) => {
         if(!option){
             return "Enter Valid Value to Add";
         }else if(this.state.options.indexOf(option) >-1){
             return "This option already exists";
         }
         this.setState((prevState)=> ({options: prevState.options.concat(option)}));
-    }
+    };
 
     render(){
         const subtitle = "Put your life in the hands of a computer";
